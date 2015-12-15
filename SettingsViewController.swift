f0b027fb14cb4a1f4af8da3cc7c97b971779a2e6
Cole McLemore <cmclemore@colemclmoresmbp.attlocal.net>
2015-12-14 22:20:07 -0600

    Commit attempt 2

--- SettingsViewController.swift
+++ SettingsViewController.swift
@@ -32,7 +32,7 @@ class SettingsViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
     
--- Tip It/ViewController.swift
+++ Tip It/ViewController.swift
@@ -89,7 +89,7 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
     @IBAction func onEditingChanged(sender: AnyObject) {
